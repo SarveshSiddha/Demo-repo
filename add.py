5883b8a5d6ed16468c5497e031b5560aac63c2b5
SarveshSiddha <siddhasarvesh@gmail.com>
2021-07-31 09:04:32 +0530

    first repository
    this is description

---
+++ add.py
@@ -0,0 +1,7 @@
+a = float(input("enter the value"))
+b = float(input("enter the value"))
+
+print("addition is : ",a+b)
+print("subtraction is : ",a-b)
+print("multiplication is : ",a*b)
+print("divion is : ",a/b)
